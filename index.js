ca046e35cb4edbb2fe17d2425a176a7695fe4d17
javeria-zohaib <161313643+javeria-zohaib@users.noreply.github.com>
2024-05-28 05:24:28 -0700

    100 days of coding

--- index.js
+++ index.js
@@ -30,3 +30,52 @@ loghobbies("baking", "reading", "coding");
 // Q50 - Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
 var myidealday = "my ideal day involve :\n1. wakingup early and go for a jog,\n2. spending quality time with my kids ,\n3. Ending day by reading a goodbook";
 console.log(myidealday);
+// Q51 - Refactoring to Arrow Functions: Take a simple function that calculates the area of a rectangle and refactor it into an arrow function.
+function calculatearea(width, height) {
+    return width * height;
+}
+var calculateareaarrow = function (width, height) {
+    return width * height;
+};
+console.log(calculateareaarrow(3, 4));
+// Q52 - Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+var smartphone = {
+    brand: "samsung",
+    model: 2021,
+    specs: {
+        storage: "64 GB",
+        screensize: "6.2 inches",
+        batterylife: "18 hrs"
+    }
+};
+console.log(smartphone);
+// Q53 - Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a computer programmer knows, like coding languages, tools, and software frameworks. Find a way to get three specific skills from this list and show them.
+var developerskills = {
+    language: ["javascript", "typescript", "python"],
+    tools: ["git", "webpack", "docker"],
+    framework: ["react", "angular", "vue"]
+};
+console.log(developerskills, "language: ".concat(developerskills.language[0], ", tools: ").concat(developerskills.tools[1], ", framework:").concat(developerskills.framework[2]));
+// Q54 - Making Flexible Object Keys: Learn how to set up a list where you can change the name of each section based on what you need at that moment, like adjusting labels based on user choices.
+function createobjectwithdynamickey(key, value) {
+    var dynamicObject = {};
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+;
+var userpreference = (createobjectwithdynamickey("theme", "dark"));
+userpreference = (createobjectwithdynamickey("student", "fatima"));
+console.log(userpreference);
+// Q55 - Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
+var numbers = [2, 3, 4, 5, 6, 7];
+var Doublenumbers = numbers.map(function (number) { return number * 2; });
+console.log(Doublenumbers);
+// Q56 - Keep Only Strings: Given a mix of different types of items, make a new list that has only the words.
+var mixedarray = [1, "apple", 2, "carrot", 3, "banana", true];
+var stringarray = mixedarray.filter(function (item) { return typeof item === "string"; });
+console.log(stringarray);
+// Q57 - Find the Average Grade: Given a list of grades, calculate the average grade.
+// A list of grades
+var grades = [88, 94, 72, 99, 53, 77];
+var averagegrades = grades.reduce(function (total, grades) { return total + grades; }, 0) / grades.length;
+console.log(averagegrades);
--- index.ts
+++ index.ts
@@ -42,3 +42,63 @@ let myidealday=`my ideal day involve :
 2. spending quality time with my kids ,
 3. Ending day by reading a goodbook`
 console.log(myidealday);
+// Q51 - Refactoring to Arrow Functions: Take a simple function that calculates the area of a rectangle and refactor it into an arrow function.
+function calculatearea (width:number,height:number){
+    return width*height
+}
+let calculateareaarrow=(width:number,height:number)=>{
+    return width*height
+}
+console.log(calculateareaarrow(3,4));
+// Q52 - Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+let smartphone={
+    brand:"samsung",
+    model:2021,
+    specs:{
+    storage:"64 GB",
+    screensize:"6.2 inches",
+    batterylife:"18 hrs"}
+
+}
+console.log(smartphone);
+// Q53 - Pulling Apart a Nested Object: Imagine you have a list inside another list that shows what a computer programmer knows, like coding languages, tools, and software frameworks. Find a way to get three specific skills from this list and show them.
+let developerskills={
+    language:["javascript","typescript","python"],
+    tools:["git","webpack","docker"],
+    framework:["react","angular","vue"]
+};
+console.log(developerskills,`language: ${developerskills.language[0]}, tools: ${developerskills.tools[1]}, framework:${developerskills.framework[2]}`)
+// Q54 - Making Flexible Object Keys: Learn how to set up a list where you can change the name of each section based on what you need at that moment, like adjusting labels based on user choices.
+function createobjectwithdynamickey (key:string,value:string) {
+    let dynamicObject={};
+    dynamicObject[key]=value;
+    return dynamicObject;
+
+};
+let userpreference=(createobjectwithdynamickey("theme","dark"));
+userpreference=(createobjectwithdynamickey("student","fatima"));
+console.log(userpreference);
+// Q55 - Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
+let numbers=[2,3,4,5,6,7];
+let Doublenumbers=numbers.map((number)=>number*2);
+console.log(Doublenumbers);
+// Q56 - Keep Only Strings: Given a mix of different types of items, make a new list that has only the words.
+let mixedarray=[1,"apple",2,"carrot",3,"banana",true];
+let stringarray=mixedarray.filter((item) =>typeof item === "string");
+console.log(stringarray);
+// Q57 - Find the Average Grade: Given a list of grades, calculate the average grade.
+// A list of grades
+
+let grades = [88, 94, 72, 99, 53, 77];
+let averagegrades=grades.reduce((total,grades )=>{return total+grades},0)/grades.length;
+console.log(averagegrades)
+
+
+
+
+
+
+
+
+
+
